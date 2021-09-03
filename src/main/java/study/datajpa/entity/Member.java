2165e6f41eaf00087e3e12eeca24ffaddaa270e3
Defian <iautm91@gmail.com>
2021-09-03 09:17:32 +0900

    예제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,43 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of={"id","username","age"})
 public class Member {
+
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private int age;
 
-    protected Member(){
-
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+
+        if(team != null){
+            chagneTeam(team);
+        }
+    }
+
+    public void chagneTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
